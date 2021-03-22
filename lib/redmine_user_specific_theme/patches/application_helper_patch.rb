12ee118377fddbbbd8c6ca94a9380ccfc5f93bb9
Austin Matzko <austin@pressedcode.com>
2021-03-21 20:24:54 -0400

    Remove deprecated Rails code.

--- lib/redmine_user_specific_theme/patches/application_helper_patch.rb
+++ lib/redmine_user_specific_theme/patches/application_helper_patch.rb
@@ -4,23 +4,23 @@ module RedmineUserSpecificTheme::Patches
   module ApplicationHelperPatch
     extend ActiveSupport::Concern
 
-    included do
-      alias_method_chain :current_theme, :user_specific
-      alias_method_chain :body_css_classes, :user_specific
+    def self.included(base)
+      base.send(:prepend, InstanceMethods)
     end
 
-    def current_theme_with_user_specific
-      user_theme = Redmine::Themes.theme(User.current.pref.ui_theme)
-      user_theme || Redmine::Themes.theme(Setting.ui_theme)
-    end
+    module InstanceMethods
+      def current_theme
+        user_theme = Redmine::Themes.theme(User.current.pref.ui_theme)
+        user_theme || Redmine::Themes.theme(Setting.ui_theme)
+      end
 
-    def body_css_classes_with_user_specific
-      css_classes = body_css_classes_without_user_specific
-      user_theme = Redmine::Themes.theme(User.current.pref.ui_theme)
-      user_theme ?
-          css_classes.gsub(/theme-\S+/, "theme-#{user_theme.name}") :
-          css_classes
+      def body_css_classes
+        css_classes = super
+        user_theme = Redmine::Themes.theme(User.current.pref.ui_theme)
+        user_theme ?
+            css_classes.gsub(/theme-\S+/, "theme-#{user_theme.name}") :
+            css_classes
+      end
     end
-
   end
 end
